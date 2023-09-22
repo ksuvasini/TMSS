3d9076397e9cce2c0c393edde6cff24b473c7c32
suvas <suvas@LAPTOP-924MOEOG>
2023-09-22 11:08:39 +0100

    Add project files.

---
+++ TMSS.DataAccess/DataContext/TMSSDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TMSS.DataAccess.DataContext
+{
+    public class TMSSDbContext : DbContext
+    {
+        public TMSSDbContext()
+        {
+        }
+
+        public TMSSDbContext(DbContextOptions<TMSSDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                throw new InvalidOperationException("Connectionstring is missing");
+            }
+        }
+    }
+}
